98a238d350c9a2dbbb96de831e1062262c1f2f81
rahulkumarbhagat-coder <bhagatrahul847@gmail.com>
2024-12-21 21:17:50 +0530

    fix links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
       <div className="wrapper">
         <motion.span initial={{opacity:0, scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}>Frontend Developer</motion.span>
         <div className="social">
-          <a href='www.linkedin.com/in/rahul-kumar-bhagat-246150313'><img src='https://cdn-icons-png.flaticon.com/512/174/174857.png'/></a>
+          <a href='https://www.linkedin.com/in/rahul-kumar-bhagat-246150313'><img src='https://cdn-icons-png.flaticon.com/512/174/174857.png'/></a>
           <a href='https://www.instagram.com/rahul.k_me/'><img src='instagram.png'/></a>
           <a href='https://github.com/rahulkumarbhagat-coder'><img src='https://icones.pro/wp-content/uploads/2021/06/icone-github-violet.png'/></a>
         </div>
